ba8789176dc1bd617ee5be409b3fab376464b0e1
landon <landon@lturner.net>
2019-03-25 11:16:09 -0500

    Disable subpath in production

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/scheduler/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/scheduler/' : '/', // Used if service lives under a subpath
   devServer: {
     proxy: 'http://localhost:1337',
   }
